0cb505e73f1c748d7d814e26f14a27420b28a0d7
ojullien <3778194+ojullien@users.noreply.github.com>
2021-07-17 22:36:45 +0200

    chore: remove old stuff
    Signed-off-by: ojullien <3778194+ojullien@users.noreply.github.com>

--- public_html/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-$bdd = new PDO('mysql:host=localhost;port=3306;dbname=pbraidersdb;charset=utf8mb4', 'pbraidersdbuser', 'pbrusrpwd', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-$resultat = $bdd->query('SELECT * FROM table');
-echo 'Il y a ' . $resultat->rowCount() . ' entrée(s) dans la base de données : </br>';
-while ($donnees = $resultat->fetch(PDO::FETCH_ASSOC)) {
-    echo $donnees['id'] . ' ' . $donnees['message'] . '</br>';
-}
